7d31e8400f2490af83d2cd1f83af541c431244f1
Gilad Ostrin <Gilado@ostrin.org>
2026-08-24 16:46:03 +0300

    feat: v1 of equipment

--- res/content/licenses/gms/li_gms.tres_ext.gd
+++ res/content/licenses/gms/li_gms.tres_ext.gd
@@ -16,5 +16,12 @@ func modify_resource(resource:Resource) -> Resource:
 	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_antishipmissile.tres'))
 	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_nexusapexhunter.tres'))
 	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_heavyslugshotgun.tres'))
+	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_recoilessrifle.tres'))
+	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_antitankmissile.tres'))
+	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_heliosnexus.tres'))
+	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_fraglauncher.tres'))
+	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_grenadelauncher.tres'))
+	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_combatmaul.tres'))
+	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_delugemissiles.tres'))
 	
 	return resource
