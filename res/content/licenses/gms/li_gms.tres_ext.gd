845b0f0ed1185a1c0e5fd36531d6936e7e194240
Gilad Ostrin <Gilado@ostrin.org>
2026-08-24 11:23:12 +0300

    feat: initial test commit

---
+++ res/content/licenses/gms/li_gms.tres_ext.gd
@@ -0,0 +1,18 @@
+extends RefCounted
+# Modifies the resource at: res://content/licenses/gms/li_gms.tres
+
+func sload(file: String):
+	if(ResourceLoader.exists(file)):
+		return ResourceLoader.load(file)
+	else:
+		return ResourceLoader.load('res://'+file.split('/res/')[1])
+
+# Apply any changes to the given resource here and they'll be saved in the virtual filesystem over the original.
+# Must return the changed resource.
+func modify_resource(resource:Resource) -> Resource:
+	var license: UnlockTree = resource
+	
+	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_autocannon.tres'))
+	license.rank_1.granted_gear.append(sload('res://unpacked/Reag-CrisisCoreCatalogEvolved/res/content/gear/gms/weapons/mw_antishipmissile.tres'))
+	
+	return resource
