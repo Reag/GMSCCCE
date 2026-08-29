700c66105e78917b6e8c0e583fb01a22e61a395c
Gilad Ostrin <Gilado@ostrin.org>
2026-08-29 11:19:52 +0300

    feat: implement shock grenades and mines

--- res/content/gear/gms/systems/ms_pattern_b_concussion_charges/concussion_mine/deployable/action_concussion_mine_detonation.gd
+++ res/content/gear/gms/systems/ms_pattern_b_concussion_charges/concussion_mine/deployable/action_concussion_mine_detonation.gd
@@ -6,7 +6,7 @@ func activate_detonation_effects(all_targets:Array[Unit], forcing_action:Specifi
 	for target:Unit in all_targets:
 		if not Unit.is_valid(target): continue
 
-		var passed_save := await UnitHasecheck.make_hull_save(activation, target, forcing_action, [EventUnitSave.FLAG.PRONE])
+		var passed_save := await UnitHasecheck.make_hull_save(activation, target, forcing_action, [Lancer.FLAG.PRONE])
 		if not passed_save:
 			UnitCondition.apply_status(activation, target, Lancer.STATUS.PRONE, Lancer.UNTIL.MANUAL, forcing_action.gear.persistent_id)
 		await activation.queue_events(CommonActionUtil.generate_knockback_events(
